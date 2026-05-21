04c88a98b7a2c48fddcd94a6c8f83a52afbf80fb
Adam <cheradam2011@yandex.ru>
2026-05-21 21:23:18 +0700

    fixed the problem with project id

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -4,7 +4,7 @@ import {projects} from "./../helpers/projectsList"
 import BtnLiveSite from "../components/btnLiveSite/btnLiveSite";
 
 const Project = () => {
-	const id = Number(window.location.href.replace("http://localhost:3000/project/", ""));
+	const id = Number(window.location.href.replace(`${window.location.origin}/project/`, ""));
 	const project = projects.filter(project => project.id === id)[0];
 	
     return (
